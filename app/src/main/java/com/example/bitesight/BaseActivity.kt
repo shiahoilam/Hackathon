070e0366fe8bcdde304e7193c9f2e8fecf40e3fe
ayethirimon1 <AYETHIRI01.23@ichat.sp.edu.sg>
2025-11-20 21:18:43 +0800

    modify bottom nav bar: add setting icon, make camera bigger and modify ui

--- app/src/main/java/com/example/bitesight/BaseActivity.kt
+++ app/src/main/java/com/example/bitesight/BaseActivity.kt
@@ -41,6 +41,11 @@ open class BaseActivity : AppCompatActivity() {
                 finish()
                 return true
             }
+            R.id.nav_settings -> {
+                startActivity(Intent(this, SettingsActivity::class.java))
+                finish()
+                return true
+            }
             else -> return false
         }
     }
@@ -51,6 +56,7 @@ open class BaseActivity : AppCompatActivity() {
             R.id.nav_analysis -> this is AnalysisActivity
             // R.id.nav_history -> this is HistoryActivity
             R.id.nav_camera -> this is CameraActivity
+            R.id.nav_settings -> this is SettingsActivity
             else -> false
         }
     }
